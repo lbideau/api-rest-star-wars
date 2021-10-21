bdfb5e69389488226f2cc0f34231f26bff662f10
lbideau <leshliebzt@gmail.com>
2021-10-20 20:22:20 -0400

    New Commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Usuario
 #from models import Person
 
 app = Flask(__name__)
@@ -20,6 +20,9 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+def swapi_to_localhost(swapi_url):
+    return swapi_url.replace("http://www.swapi.tech/api/","http://localhost:3000/")
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,14 +33,58 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/usuarios', methods=['GET'])
+def handle_usuarios():
+    usuarios = Usuario.query.all()
+    print(usuarios)
+    response = []
+    for usuario in usuarios:
+        response.append(usuario.serialize())
+    print(response)
+    return jsonify(response),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/usuarios/<int:usuario_id>/favoritos/<string:nature>', methods=['POST'])
+def handle_users_favorite(user_id,nature):
+    uid = request.json['uid']
+    nuevo_favorito = FavoritoUsuario (
+        usuario_id = usuario_id,
+        url = f"http://swapi.tech/api/{nature}/{uid} "
+    )
+    db.session.add(nuevo_favorito)
+    try:
+        db.session.commit()
+        return jsonify(nuevo_favorito.serialize()),201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(error.args),500
 
-    return jsonify(response_body), 200
+@app.route('/people',methods=['GET'])
+def handle_people():
+    limit = request.args.get("limit", "10")
+    page = requests.args.get("page",1)
+    response = request.get(f'https://swapi.dev/api/people?page={page}&limit={limit}')
+    print(response.status_code)
+    response = response.json()
+    response.update(
+        previous = swapi_to_localhost(response['previous']) if response['previous'] else None,
+        next = swapi_to_localhost(response['next']) if response['next'] else None
+    )
+
+    
+    return jsonify([]),200
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+    response = request.get(f'https://swapi.dev/api/planets?page=1&limit=80') #Tiene la respusta de la solicitud de esa url
+    response = request.json() #Devuelve un diccionario
+    print(response['results'])
+    return jsonify([]),200 # Muestra la respuesta en formato json
+
+    #response_body = {
+     #   "msg": "Hello, this is your GET /user response "
+    #}
+
+   # return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+    name = db.Column(db.String(250))
+    last_name = db.Column(db.String(10))
+    user_name = db.Column(db.String(30), unique=True)
+    #email = db.Column(db.String(80),nullable=False, unique=True)
+    #password = db.Column(db.String(80),unique=False, nullable=False)
+    favoritos = db.relationship("FavoritoUsuario", backref = "usuario", uselist = True)
     def serialize(self):
-        return {
+        return{
+            "user_name": self.user_name,
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last_name": self.last_name
         }
+class FavoritoUsuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(125), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("usuario.id"),nullable=False)
+    __table_args__ = (db.UniqueConstraint(
+        "user_id",
+        "url",
+        name = "unico_favorito_para_usuario"
+    ),)
+    
+    def serialize(self):
+        return{
+            "usuario_id" : self.usuario_id,
+            "url": self.url,
+            "id" : self.id
+        }
+
+    
+
+  
